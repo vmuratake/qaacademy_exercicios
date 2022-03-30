94c1bc2de289924fe2a27715e67541f62a922d6b
Vanessa Muratake <vanessamuratake@hotmail.com>
2022-03-29 21:47:48 -0300

    Ex2 refatoração

---
+++ src/easy/Ex02execucao.java
@@ -0,0 +1,9 @@
+package easy;
+
+public class Ex02execucao {
+    public static void main(String[] args) {
+        Exercicio02 exercicio02 = new Exercicio02();
+        System.out.println(
+            exercicio02.retornaPalavraDigitada("Palavra Teste"));
+    }
+}
--- src/easy/Exercicio02.java
+++ src/easy/Exercicio02.java
@@ -1,12 +1,7 @@
 package easy;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio02 {
-    public static void main(String[] args) {
-        String palavraDigitada;
-        palavraDigitada = JOptionPane.showInputDialog("Digite uma palavra");
-        System.out.println("Você digitou: " +palavraDigitada);
-        
+    public String retornaPalavraDigitada (String palavraDigitada) {
+        return "Você digitou: " +palavraDigitada;
     }
 }
