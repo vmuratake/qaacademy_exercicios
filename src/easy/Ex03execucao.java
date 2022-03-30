e5ce6d5d9320f533bc0399446432fe921c377785
Vanessa Muratake <vanessamuratake@hotmail.com>
2022-03-29 22:12:06 -0300

    Ex3 refatoração

---
+++ src/easy/Ex03execucao.java
@@ -0,0 +1,9 @@
+package easy;
+
+public class Ex03execucao {
+    public static void main(String[] args) {
+        Exercicio03 exercicio03 = new Exercicio03();
+        System.out.println(exercicio03.retornaNumero1());
+        System.out.println(exercicio03.retornaNumero2());
+    }
+}
--- src/easy/Exercicio03.java
+++ src/easy/Exercicio03.java
@@ -1,25 +1,11 @@
 package easy;
 
-
-import javax.swing.JOptionPane;
-
 public class Exercicio03 {
-    public static void main(String[] args) {
-        //String  primeiroNumero;
-        int numero1;
-        //String segundoNumero;
-        int numero2;
-
-        //primeiroNumero = JOptionPane.showInputDialog("Digite um número: ");
-        numero1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número"));
-        //segundoNumero = JOptionPane.showInputDialog("Digite outro número: ");
-        //numero2 = Double.parseDouble(JOptionPane.showInputDialog("Digite outro número"));
-        numero2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro número"));
-        //System.out.println("O valor do primeiro número é: " +segundoNumero);
-        //System.out.println("O valor do segundo número é: " +primeiroNumero);
-
-        System.out.println("O valor do segundo número é: " +numero2);
-        System.out.println("O valor do segundo número é: " +numero1);
+    public String retornaNumero1 (){
+        return "Exibe numero";
     }
-    
+
+    public String retornaNumero2 (){
+        return "Exibe numero";
+    }   
 }
