38e224aa33435edef88accc476a555bdea9ba494
Vanessa Muratake <vanessamuratake@hotmail.com>
2022-04-02 10:38:29 -0300

    Refatoração

--- src/easy/Ex02execucao.java
+++ src/easy/Ex02execucao.java
@@ -3,7 +3,6 @@ package easy;
 public class Ex02execucao {
     public static void main(String[] args) {
         Exercicio02 exercicio02 = new Exercicio02();
-        System.out.println(
-            exercicio02.retornaPalavraDigitada("Palavra Teste"));
+        System.out.println(exercicio02.retornaPalavraDigitada("Palavra Teste"));
     }
 }
---
+++ src/easy/Ex04execucao.java
@@ -0,0 +1,13 @@
+package easy;
+
+public class Ex04execucao {
+
+    public static void main(String[] args) {
+        Ex04execucao exercicio4 = new Ex04execucao();
+        System.out.println(exercicio4.retornaDobraNum());
+    }
+
+    private char[] retornaDobraNum() {
+        return null;
+    }
+}
