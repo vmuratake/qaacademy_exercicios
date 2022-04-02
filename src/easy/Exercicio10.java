9aa3691ccc7c7fb7405d53d60e2606f599ed1235
Vanessa Muratake <vanessamuratake@hotmail.com>
2022-04-02 16:00:47 -0300

    Refatoração exe10

--- src/easy/Exercicio10.java
+++ src/easy/Exercicio10.java
@@ -1,15 +1,12 @@
 package easy;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio10 {
-    public static void main(String[] args) {
-        
-        double valorTotal, valorJuros= 0, taxaJuros= 0.05;
+    public double  calcularInvestimento( double valorInvestimento, double taxaJuros) {
+        double valorJuros = 0;
         int i=1;
 
-        String valorInveString = JOptionPane.showInputDialog("O valor do investimento será de R$ ");
-        Double valorInve = Double.parseDouble(valorInveString);
+        // String valorInveString = JOptionPane.showInputDialog("O valor do investimento será de R$ ");
+        // Double valorInve = Double.parseDouble(valorInveString);
 
         //Sem While:
         // taxaJuros= 0.05*10;
@@ -18,15 +15,14 @@ public class Exercicio10 {
 
 
         while (i <= 10) {
-            valorJuros = (valorInve * taxaJuros) + valorJuros;
+            valorJuros = (valorInvestimento * taxaJuros) + valorJuros;
             i++;
         }
+        return valorJuros;
 
-        valorTotal = valorJuros + valorInve;
 
-        System.out.println("Você investiu R$ " +valorInve);
-        System.out.println("Rendeu R$ " +valorJuros+ " de Juros.");   
-        System.out.println("O valor total são R$ " +valorTotal);
-
+        // valorTotal = valorJuros + valorInvestimento;
     }
+
+    
 }
