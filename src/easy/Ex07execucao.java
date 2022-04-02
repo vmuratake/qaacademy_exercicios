d8013961b3f60e0547e59aac3fcd764317208de7
Vanessa Muratake <vanessamuratake@hotmail.com>
2022-04-02 14:35:03 -0300

    Refatoração exe07

---
+++ src/easy/Ex07execucao.java
@@ -0,0 +1,8 @@
+package easy;
+
+public class Ex07execucao {
+    public static void main(String[] args) {
+        Exercicio07 exercicio07 = new Exercicio07();
+        System.out.println("O valor do Inss a ser pago será: " + exercicio07.calcularInss(3000.00));
+    }
+}
--- src/easy/Exercicio07.java
+++ src/easy/Exercicio07.java
@@ -1,39 +1,31 @@
 package easy;
 
-import javax.swing.JOptionPane;
-
 public class Exercicio07 {
-    public static void main(String[] args) {
+    public double calcularInss (double salario) {
 
-        double inss;
-
-        String salarioString = JOptionPane.showInputDialog("Qual o valor do salário R$ ");
-        double salario = Double.parseDouble(salarioString);
+        double inss = 0;
 
         if (salario <= 1045.00) {
             inss = salario*0.075;
-            System.out.println("O valor do INSS será R$  " +inss);
         }
 
         if (salario > 1045.01 && salario <= 2089.60) {
             inss = salario*0.09;
-            System.out.println("O valor do INSS será R$  " +inss);
         }
 
         if  (salario >= 2089.61 && salario <= 3134.40) {
             inss = salario*0.12;
-            System.out.println("O valor do INSS será R$  " +inss);
         }
 
         if  (salario >= 3134.41 && salario <= 6101.06) {
             inss = salario*0.14;
-            System.out.println("O valor do INSS será R$  " +inss);
         }
 
         if  (salario >= 6101.07) {
             inss = 854.15;
-            System.out.println("O valor do INSS será R$  " +inss);
         }
+
+        return inss;
 
     }
     
